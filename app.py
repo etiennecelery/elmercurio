3eb4ea17ad6b9f01b3adce5028e4def0d16bd712
etiennecelery <etiennecelery@gmail.com>
2020-09-30 16:16:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def get_images(cuerpo, date):
     return div
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=1112, host='0.0.0.0')
+    app.run_server(debug=False, port=1112, host='0.0.0.0')
